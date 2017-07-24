36da9f9eea8207c2c40edb406b7d93194b3ea4f5
Syrimande <caterinadasilva@gmail.com>
2017-07-24 10:17:20 -0400

    nav y header responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 $(document).ready( function(){
 	$(".dropdown-button").dropdown();
 	$(".button-collapse").sideNav();
-	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true,
+		indicators: false
+	});
+	   // move next carousel
+   $('.moveNextCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('next');
+   });
+
+   // move prev carousel
+   $('.movePrevCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('prev');
+   });
+	setInterval(function() {
+	    $('.carousel').carousel('next');
+	}, 5000); // every 2 seconds
 });
